6d126ede104f004676704346fb01647437292782
Kyan <kyangyamada@gmail.com>
2024-09-08 00:02:02 -0300

    Update
    Atualização dos arquivos para correção de bugs.

--- app.js
+++ app.js
@@ -36,6 +36,52 @@ function pesquisar() {
             </div>
             `;
         }
+        //Muda os nomes de civis na base de dados para letras maiúsculas.
+        Civil = dado.nomeCivil.toUpperCase()
+        //Encontra se o nome de civil do herói pesquisado está no banco de dados.
+        if (Civil.includes(caixaPesquisa)) {
+            // Cria um novo elemento de acordo com pesquisa
+            resultados += `
+            <div class="dossie">
+                <img src="dossies-herois/${dado.imagem}" width="270" height="360" align="right" />
+                <h2>${dado.alterEgo}</h2>
+                <h3>${dado.nomeCivil}</h3>
+                <p>
+                    <b>Estado de atividade:</b> ${dado.atividade}<br>
+                    <b>Organização:</b> ${dado.organizacao}<br>
+                    <b>Cidade de atuação:</b> ${dado.cidade}<br>
+                    <b>Arqui-inimigo:</b> ${dado.vilao}<br>
+                    <b>Habilidades:</b> ${dado.poder}<br>
+                    <b>Fraqueza:</b> ${dado.fraqueza}<br>
+                    <b>Nível de Risco:</b> ${dado.risco}<br>
+                    <b>Análise psicológica:</b> ${dado.analise}
+                </p>
+            </div>
+            `;
+        }
+        //Muda os nomes das organizações na base de dados para letras maiúsculas.
+        Organizacao = dado.organizacao.toUpperCase()
+        //Encontra os membros de determinada organização no banco de dados.
+        if (Organizacao.includes(caixaPesquisa)) {
+            // Cria um novo elemento de acordo com pesquisa
+            resultados += `
+            <div class="dossie">
+                <img src="dossies-herois/${dado.imagem}" width="270" height="360" align="right" />
+                <h2>${dado.alterEgo}</h2>
+                <h3>${dado.nomeCivil}</h3>
+                <p>
+                    <b>Estado de atividade:</b> ${dado.atividade}<br>
+                    <b>Organização:</b> ${dado.organizacao}<br>
+                    <b>Cidade de atuação:</b> ${dado.cidade}<br>
+                    <b>Arqui-inimigo:</b> ${dado.vilao}<br>
+                    <b>Habilidades:</b> ${dado.poder}<br>
+                    <b>Fraqueza:</b> ${dado.fraqueza}<br>
+                    <b>Nível de Risco:</b> ${dado.risco}<br>
+                    <b>Análise psicológica:</b> ${dado.analise}
+                </p>
+            </div>
+            `;
+        }
         if (caixaPesquisa == "") {
             // Cria um novo elemento com toda a base de dados
             resultados += `
@@ -55,6 +101,9 @@ function pesquisar() {
                     </p>
                 </div>
                 `;
+        }
+        if (!resultados) {
+            resultados = "Nenhum herói encontrado."
         }
     }
 
